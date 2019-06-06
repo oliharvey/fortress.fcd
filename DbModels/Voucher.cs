108fb823cfce067da8be08cadc87e3419eaa9eaf
Fiona Titcombe <fiona@n51solutions.co.uk>
2019-06-06 13:34:36 +0100

    Extra field on voucher

--- DbModels/Voucher.cs
+++ DbModels/Voucher.cs
@@ -103,6 +103,9 @@ namespace FortressCodesDomain.DbModels
         [StringLength(20)]
         public String PPPostcode { get; set; }
 
+        [StringLength(50)]
+        public String ThirdPartyReference { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Transaction> Transactions { get; set; }
 
